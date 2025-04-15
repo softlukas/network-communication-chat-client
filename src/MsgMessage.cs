9d90cb38a32a8e1d961b39403b633c6d2329d0f4
xsimonl00 <xsimonl00@stud.fit.vutbr.cz>
2025-04-16 01:28:14 +0200

    fix MsgMessage: grammer conntent specification

--- src/MsgMessage.cs
+++ src/MsgMessage.cs
@@ -1,16 +1,29 @@
 using System.Text;
+using System.Net;
 namespace Ipk25Chat
 {
-    // Represents a standard MSG chat message data container.
     public class MsgMessage : Message
     {
-        // Gets the message type, which is always MSG for this class.
+        // Gets the message type, which is always MSG for this class
         public override MessageType Type => MessageType.MSG;
         
         public string DisplayName { get; private set; }
 
-        
-        public string MessageContent { get; private set; }
+        private string _messageContent;
+        public string MessageContent {
+        get {
+
+            return _messageContent;
+
+        } private set {
+            if(!IsValidContent(value))
+            {
+                throw new ArgumentException("ERROR: Invalid message content.");
+            }
+            _messageContent = value;
+        }
+        }
+
 
         public MsgMessage(string displayName, string messageContent)
         {
@@ -19,21 +32,70 @@ namespace Ipk25Chat
             MessageContent = messageContent;
         }
 
-        public override byte[] GetTcpPayload()
+        public MsgMessage(string displayName, string messageContent, int messageId) : base(messageId)
+        {
+            // Initialize the message content with messageId (udp)
+            DisplayName = displayName;
+            MessageContent = messageContent;
+        }
+
+        
+        
+        private static bool IsValidContent(string? content)
+        {
+            if (string.IsNullOrEmpty(content) || content.Length > 60000)
+            {
+                return false;
+            }
+
+            return content.All(c => (c >= '!' && c <= '~') || c == ' ' || c == '\n');      
+        }
+        
+
+        public override byte[] GetBytesInTcpGrammar()
         {
             // Format the message as "MSG FROM {DisplayName} IS {MessageContent}"
             string dataString = $"MSG FROM {this.DisplayName} IS {this.MessageContent}\r\n";
            
             byte[] dataBytes = Encoding.ASCII.GetBytes(dataString);
 
-            // Return the byte array
+           
             return dataBytes;
+        }
+
+        public override byte[] GetBytesForUdpPacket()
+        {
+            
+            using (var memoryStream = new MemoryStream())
+            
+            using (var writer = new BinaryWriter(memoryStream, Encoding.ASCII, false))
+            {
+                
+                writer.Write((byte)this.Type); // 0x04
+
+               
+                short networkOrderMessageId = IPAddress.HostToNetworkOrder((short)this.MessageId);
+
+                // messageID without 0
+                writer.Write(networkOrderMessageId);
+
+                // displayName + 0
+                WriteNullTerminated(writer, this.DisplayName);
+
+                // Message content + 0
+                WriteNullTerminated(writer, this.MessageContent);
+
+               
+                writer.Flush();
+
+                // Return the contents of the MemoryStream as a byte array
+                return memoryStream.ToArray();
+            }
         }
 
         public override string ToString()
         {
-            // Format the message as "MSG FROM {DisplayName} IS {MessageContent}"
-            return $"Server: {DisplayName} {MessageContent}\n";
+            return $"{DisplayName}: {MessageContent}\n";
         }
     }
 }

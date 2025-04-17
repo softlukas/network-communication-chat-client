5e66f34686a2e2ffa2690ee223ee42ac57b299aa
xsimonl00 <xsimonl00@stud.fit.vutbr.cz>
2025-04-17 18:21:30 +0200

    fix ByeMessage: add constr overload with message id, add function to get bytes in udp

--- src/ByeMessage.cs
+++ src/ByeMessage.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Net;
 namespace Ipk25Chat
 {
     public class ByeMessage : Message // Dedí od základnej triedy Message
@@ -14,6 +15,13 @@ namespace Ipk25Chat
         {
             DisplayName = displayName;
         }
+
+        public ByeMessage(string displayName, int messageId) : base (messageId)
+        {
+            DisplayName = displayName;
+        }
+
+        
         public override byte[] GetBytesInTcpGrammar()
         {
             // Format the message as "BYE {DisplayName}\r\n"
@@ -25,6 +33,34 @@ namespace Ipk25Chat
             // Return the byte array
             return dataBytes;
         }
+
+         public override byte[] GetBytesForUdpPacket()
+        {
+            
+            using (var memoryStream = new MemoryStream())
+            
+            using (var writer = new BinaryWriter(memoryStream, Encoding.ASCII, false))
+            {
+                
+                writer.Write((byte)this.Type);
+
+               
+                short networkOrderMessageId = IPAddress.HostToNetworkOrder((short)this.MessageId);
+
+                // messageID without 0
+                writer.Write(networkOrderMessageId);
+
+                // displayName + 0
+                WriteNullTerminated(writer, this.DisplayName);
+
+                writer.Flush();
+
+                // Return the contents of the MemoryStream as a byte array
+                return memoryStream.ToArray();
+            }
+        }
+
+        
         public override string ToString()
         {
             // Format the message as "BYE {DisplayName}"

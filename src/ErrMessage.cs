9d80ebc8c2f65a85f8fb8e672bf8e8a6c7875922
xsimonl00 <xsimonl00@stud.fit.vutbr.cz>
2025-04-19 00:54:37 +0200

    fix ErrMessage: add missing function to get bytes in tcp grammer and bytes for udp packet

--- src/ErrMessage.cs
+++ src/ErrMessage.cs
@@ -1,4 +1,5 @@
-
+using System.Net;
+using System.Text;
 using System;
 
 namespace Ipk25Chat
@@ -34,6 +35,47 @@ namespace Ipk25Chat
         {
             // Format the message as "ERR {DisplayName} IS {MessageContent}"
             return $"ERROR FROM {this.DisplayName}: {this.MessageContent}\n";
+        }
+
+        public override byte[] GetBytesInTcpGrammar()
+        {
+            // Format the message as "ERR {DisplayName} IS {MessageContent}\r\n"
+            string dataString = $"ERR FROM {this.DisplayName} IS {this.MessageContent}\r\n";
+            
+            // Convert the string to a byte array using ASCII encoding
+            byte[] dataBytes = Encoding.ASCII.GetBytes(dataString);
+            
+            return dataBytes;
+        }
+
+        public override byte[] GetBytesForUdpPacket()
+        {
+            
+            using (var memoryStream = new MemoryStream())
+            
+            using (var writer = new BinaryWriter(memoryStream, Encoding.ASCII, false))
+            {
+                
+                writer.Write((byte)this.Type);
+
+               
+                short networkOrderMessageId = IPAddress.HostToNetworkOrder((short)this.MessageId);
+
+                // messageID without 0
+                writer.Write(networkOrderMessageId);
+
+                // displayName + 0
+                WriteNullTerminated(writer, this.DisplayName);
+
+                // Message content + 0
+                WriteNullTerminated(writer, this.MessageContent);
+
+               
+                writer.Flush();
+
+                // Return the contents of the MemoryStream as a byte array
+                return memoryStream.ToArray();
+            }
         }
     }
 }
